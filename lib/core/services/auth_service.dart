c02f4108ba4b5983ad83e370aa9fe6bd5cec240f
Rafat <rafat.mohammed9090@gmail.com>
2026-01-03 11:03:35 +0300

    replace mock auth with firebase auth

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,13 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
-  String? _currentUserId;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  String? get currentUserId => _currentUserId;
+  User? get currentUser => _auth.currentUser;
 
-  void signInMock(String userId) {
-    _currentUserId = userId;
+  Future<UserCredential> signInWithEmail({
+    required String email,
+    required String password,
+  }) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 
-  void signOut() {
-    _currentUserId = null;
+  Future<UserCredential> registerWithEmail({
+    required String email,
+    required String password,
+  }) {
+    return _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() {
+    return _auth.signOut();
   }
 }
