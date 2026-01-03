a4b77a3a2bf3dfc5b111f838ba794d595e2e73bf
Rafat <rafat.mohammed9090@gmail.com>
2026-01-03 10:35:16 +0300

    add base user model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,17 @@
+class UserModel {
+  final String id;
+  final String fullName;
+  final String phone;
+  final String email;
+  final String departmentId;
+  final String role;
+
+  UserModel({
+    required this.id,
+    required this.fullName,
+    required this.phone,
+    required this.email,
+    required this.departmentId,
+    required this.role,
+  });
+}
